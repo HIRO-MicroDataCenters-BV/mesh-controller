87bab3347512e1f939e0fbe28c47b099503dd45d
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2025-09-12 10:21:34 +0200

    EDGE-252 upgrading tool chain

--- mesh/src/metrics.rs
+++ mesh/src/metrics.rs
@@ -1,2 +1,11 @@
-pub const MESSAGE_RECEIVE_TOTAL: &str = "message_receive_total";
+// Labels
 pub const LABEL_MSG_TYPE: &str = "msg_type";
+pub const LABEL_MSG_TYPE_SNAPSHOT: &str = "snapshot";
+pub const LABEL_MSG_TYPE_EVENT: &str = "event";
+
+/// Metrics
+pub const LAST_TIMESTAMP: &str = "last_timestamp";
+pub const MESSAGE_RECEIVE_TOTAL: &str = "message_receive_total";
+pub const RESOURCE_COUNT: &str = "resource_count";
+pub const ACTIVE_PEER_COUNT: &str = "active_peer_count";
+pub const MEMBERSHIP_CHANGE_TOTAL: &str = "membership_change_total";
