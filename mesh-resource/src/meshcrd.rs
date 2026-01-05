949730ace7ceba5a79f8e48b9d5d119fd3fb7945
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2026-01-05 10:07:25 +0100

    EDGE-375 library upgrades

--- mesh-resource/src/meshcrd.rs
+++ mesh-resource/src/meshcrd.rs
@@ -4,7 +4,7 @@ use meshresource::meshpeer::MeshPeer;
 
 pub fn main() -> Result<()> {
     let crd = MeshPeer::crd();
-    let yaml = serde_yaml::to_string(&crd).unwrap();
+    let yaml = serde_yaml_bw::to_string(&crd).unwrap();
     println!("{}", yaml);
     Ok(())
 }
