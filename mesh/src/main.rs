23cb386cba2eef99942a382e5343a1a0f29d8e25
ktatarnikov <kostyantyn.tatarnikov@gmail.com>
2026-01-08 16:43:05 +0100

    EDGE-352 adding connection driver and connection create/drop metrics

--- mesh/src/main.rs
+++ mesh/src/main.rs
@@ -12,6 +12,8 @@ fn main() -> Result<()> {
         metrics.insert(iroh::metrics::ConnectionMetrics::new(reg));
         metrics.insert(iroh::metrics::MagicsockMetrics::new(reg));
         metrics.insert(iroh::metrics::NetReportMetrics::new(reg));
+        metrics.insert(iroh_quinn::metrics::ConnectionDriverMetrics::new(reg));
+        metrics.insert(iroh_quinn::metrics::QuinnConnectionMetrics::new(reg));
     });
 
     let context_builder = ContextBuilder::from_cli()?;
